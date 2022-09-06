3677b89ba96e784127d5366cad0332cf502a8146
1994Khushi <ayangelyoung@gmai.com>
2022-09-06 16:50:51 +0530

    Multiply

---
+++ src/com/Multiply.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Multiply {
+
+    public static void main(String[] args) {
+
+        int a=20;
+        int b=37;
+        int c=12;
+
+        int d;
+
+        d = a * b * c;
+
+        System.out.println("Result" + d);
+
+    }
+}
