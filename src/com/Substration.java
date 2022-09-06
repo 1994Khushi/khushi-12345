1036405c0aa84088558de9ac9398d59a934d5353
1994Khushi <ayangelyoung@gmai.com>
2022-09-06 16:46:50 +0530

    khushi comit

---
+++ src/com/Substration.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Substration {
+    public static void main(String[] args) {
+
+        int a=34;
+        int b=15;
+        int c;
+
+        c=a-b;
+
+        System.out.println("substration" + c);
+    }
+}
