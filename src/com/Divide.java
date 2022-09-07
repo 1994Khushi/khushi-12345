bf4e2c7ea68c294a00d7968f4cd783b7270b44d0
1994Khushi <ayangelyoung@gmai.com>
2022-09-07 16:57:08 +0530

    divide

---
+++ src/com/Divide.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Divide {
+
+
+    public static void main(String[] args) {
+        int a=24;
+        int b=6;
+
+        int c;
+
+        c= a/b;
+
+        System.out.println("result" +c);
+
+    }
+}
